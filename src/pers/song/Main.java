6ee0ca6ed24f26896aaa24494f13026279e04ff5
song <309222152@qq.com>
2019-04-08 23:43:10 +0800

    实现简单解法

--- src/pers/song/Main.java
+++ src/pers/song/Main.java
@@ -6,6 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
         int[][] input = new int[9][9];
+//        int[][] input = {{2,3,9,0,1,8,4,0,0},
+//                         {5,1,6,3,4,7,9,0,8},
+//                         {7,8,0,6,2,9,3,5,1},
+//                         {0,0,7,1,0,4,5,6,3},
+//                         {6,0,3,7,5,2,1,8,0},
+//                         {8,5,0,9,6,3,7,0,2},
+//                         {1,7,0,2,3,0,8,9,4},
+//                         {3,6,8,4,9,0,2,0,7},
+//                         {4,0,2,8,0,1,0,3,5}
+//                        };
         Scanner in = new Scanner(System.in);
 
         for (int i = 0 ; i < 9 ; i ++){
@@ -18,6 +28,7 @@ public class Main {
             for(int j = 0 ; j < 9 ; j++){
                 input[i][j] = Integer.parseInt( s.charAt(j)+"" );
             }
+
         }
 
         SudokuSolution sudokuSolution = new SudokuSolution(input);
--- src/pers/song/SudokuSolution.java
+++ src/pers/song/SudokuSolution.java
@@ -1,5 +1,10 @@
 package pers.song;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SudokuSolution {
     /**
      * 输入格式：0代表空格，需要确定值
@@ -41,7 +46,36 @@ public class SudokuSolution {
                     0, inputArr[i].length);
         }
         System.out.println("待求解的数独：");
-        printSudoku(resultArr);
+        printSudoku(inputArr);
+        boolean flag = false ;
+        boolean done = false ;
+        do {
+            flag = false;
+            for(int i = 0 ; i < 9 ; i++){
+                for (int j = 0 ; j < 9 ; j++){
+                    if(resultArr[i][j] != 0) continue;
+                    int[] possibleArr = checkPossible(i,j);
+                    //只有一个元素的即可确定填入
+                    if(possibleArr.length == 1){
+                        int value = possibleArr[0];
+                        resultArr[i][j] = value ;
+                        flag = true;
+                    }
+
+                }
+            }
+            done = checkDone();
+
+        }while (flag && !done);
+
+        done = checkDone();
+        if(done){
+            System.out.println("成功求解：");
+            printSudoku(resultArr);
+        }else{
+            System.out.println("未能成功求解：");
+            printSudoku(resultArr);
+        }
 
         return 0;
     }
@@ -67,6 +101,85 @@ public class SudokuSolution {
             System.out.print("| \r\n");
         }
         System.out.println("- - - - - - - - - - - - -");
+    }
+
+    /**
+     * 获取一个空格所在的宫中已填入的数据
+     * @return
+     */
+    private int[] getSubGrids(int i ,int j){
+        int istart = (i / 3) * 3 ;
+        int jstart = (j / 3) * 3 ;
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(int ii = istart ; ii < istart + 3 ; ii ++){
+            for(int jj = jstart ; jj < jstart + 3 ; jj++){
+                if(resultArr[ii][jj] != 0){
+                    int value = resultArr[ii][jj];
+                    result.add(value);
+                }
+            }
+        }
+
+        //List转Int[]的方法 jdk8
+        return result.stream().mapToInt(Integer::valueOf).toArray();
+    }
+
+    /**
+     * 获取一个空格所有可能填入的值
+     * @return
+     */
+    private int[] checkPossible(int i , int j){
+        if(resultArr[i][j] != 0){
+            //如果不为0，说明已经填入的，则返回本身
+            return new int[]{resultArr[i][j]};
+        }
+
+        List<Integer> possibleList = Arrays.stream( possibleArr ).boxed().collect(Collectors.toList());
+
+        //1.检查该行和该列
+        for(int ii = 0 ; ii < 9 ; ii ++){
+            if(resultArr[ii][j] == 0) continue;
+            if(possibleList.contains(resultArr[ii][j])){
+                int index = possibleList.indexOf(resultArr[ii][j]);
+                possibleList.remove(index);
+            }
+        }
+
+        for(int jj = 0 ; jj < 9 ; jj ++){
+            if(resultArr[i][jj] == 0) continue;
+            if(possibleList.contains(resultArr[i][jj])){
+                int index = possibleList.indexOf(resultArr[i][jj]);
+                possibleList.remove(index);
+            }
+        }
+
+        //2.检查宫内的数据
+        int[] subgrids = getSubGrids(i,j);
+        for(int k = 0 ; k < subgrids.length ; k ++){
+            if(subgrids[k] == 0 ) continue;
+            if(possibleList.contains(subgrids[k])){
+                int index = possibleList.indexOf(subgrids[k]);
+                possibleList.remove(index);
+            }
+
+        }
+
+        return possibleList.stream().mapToInt(Integer::valueOf).toArray();
+
+    }
+
+    private boolean checkDone(){
+        boolean flag = true;
+        for(int i = 0 ; i < 9 ; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (resultArr[i][j] == 0) {
+                    flag = false ;
+                    break;
+                }
+            }
+        }
+
+        return flag;
     }
 
 }
